ab071647568255b7c80c776beaaf467168b4f5ec
Carl Smedstad <carl.smedstad@protonmail.com>
2024-08-24 14:28:06 +0200

    Compile powerline executable without setuptools

--- setup.py
+++ setup.py
@@ -21,40 +21,7 @@ except IOError:
 OLD_PYTHON = sys.version_info < (2, 7)
 
 
-def compile_client():
-	'''Compile the C powerline-client script.'''
-
-	if hasattr(sys, 'getwindowsversion'):
-		raise NotImplementedError()
-	else:
-		from distutils.ccompiler import new_compiler
-		compiler = new_compiler().compiler
-		cflags = os.environ.get('CFLAGS', str('-O3'))
-		# A normal split would do a split on each space which might be incorrect. The
-		# shlex will not split if a space occurs in an arguments value.
-		subprocess.check_call(compiler + shlex.split(cflags) + ['client/powerline.c', '-o', 'scripts/powerline'])
-
-try:
-	compile_client()
-except Exception as e:
-	print('Compiling C version of powerline-client failed')
-	logging.exception(e)
-	# FIXME Catch more specific exceptions
-	import shutil
-	if hasattr(shutil, 'which'):
-		which = shutil.which
-	else:
-		sys.path.append(CURRENT_DIR)
-		from powerline.lib.shell import which
-	can_use_scripts = True
-	if which('socat') and which('sed') and which('sh'):
-		print('Using powerline.sh script instead of C version (requires socat, sed and sh)')
-		shutil.copyfile('client/powerline.sh', 'scripts/powerline')
-	else:
-		print('Using powerline.py script instead of C version')
-		shutil.copyfile('client/powerline.py', 'scripts/powerline')
-else:
-	can_use_scripts = False
+can_use_scripts = False
 
 setup(
 	name='powerline-status',
